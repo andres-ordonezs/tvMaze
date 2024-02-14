e078bc099f24b2ed91e3b3c481e298674ade9e89
andres-ordonezs <andresmw.89@gmail.com>
2024-02-14 15:19:40 -0500

    Step 1-4 solved

--- tvmaze.js
+++ tvmaze.js
@@ -3,7 +3,8 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
-
+//TODO: Add base URL as a global constant
+//TODO: DEFAULT IMAGE URL CONSTANT
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -12,27 +13,40 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(showName) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const response = await fetch(`https://api.tvmaze.com/search/shows?q=${showName}`);
+  const showData = await response.json();
+  console.log(showData);
+  return showData.map(show => {
+    return ({
+      id: show.show.id,
+      name: show.show.name,
+      summary: show.show.summary === null ? 'No Summary Available' : show.show.summary,
+      image: show.show.image === null ? 'https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300' : show.show.image.medium,
+    }
+    );
+  });
+  /* console.log(showData); */
 
-  return [
+  /* return [
     {
       id: 1767,
       name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+      summary:∫
+        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
            women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
+         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+           normal lives, modestly setting aside the part they played in
+           producing crucial intelligence, which helped the Allies to victory
            and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
+           unsolved murder she is met by skepticism from the police. She
            quickly realises she can only begin to crack the murders and bring
            the culprit to justice with her former friends.</p>`,
       image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
     }
-  ]
+  ]; */
 }
 
 
@@ -48,9 +62,9 @@ function displayShows(shows) {
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src=${show.image}
+              alt=${show.name}
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -80,7 +94,7 @@ async function searchShowsAndDisplay() {
   displayShows(shows);
 }
 
-$searchForm.on("submit", async function handleSearchForm (evt) {
+$searchForm.on("submit", async function handleSearchForm(evt) {
   evt.preventDefault();
   await searchShowsAndDisplay();
 });
